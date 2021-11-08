943ac9144fec014961d646f4a8561525c2f95347
sn77sohan <sohan.ahmedself@gmail.com>
2021-11-08 15:28:43 +0600

    react-core-concept-recape-first-edition

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MyComponent brand="Apple" price="
+    170000"></MyComponent>
+    <MyComponent brand="Microsoft" price="10000"></MyComponent>
+    <MyComponent brand="Google" price="0"></MyComponent>
+    <MyComponent></MyComponent>
     </div>
   );
 }
 
+
+function MyComponent(props){
+  console.log(props);
+  
+  const myStyle={
+    backgroundColor: 'lightblue',
+    border: '3px solid blue',
+    margin: '20px',
+    padding: '10px'
+  }
+  return(
+    <div style={myStyle}>
+      <h1>Brand Name: {props.brand}</h1>
+      <p style={{color: 'magenta', fontWeight: 'bold'}}>Brand value: {props.price}</p>
+
+    </div>
+  )
+}
 export default App;
